98182925bd7111351b9ba84f46e6f6f74f66ab9c
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-22 23:41:55 +0000

    completed word search for horizontal and vertical

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    let array = [];
+    for (let i = 0; i < letters.length; i++) {
+        for (let j = 0; j < letters[i].length; j++) {
+            if (i === 0) {
+                array.push([letters[i][j]])
+            } else {
+                array[j].push(letters[i][j])
+            }
+        }
     }
+    
+    const verticalJoin = array.map(ls => ls.join(''))
+    for (line of verticalJoin) {
+        if (line.includes(word)) {
+            return true;
+        }
+    }
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (line of horizontalJoin) {
+        if (line.includes(word)) {
+            return true;
+        } 
+    }
+
+    return false;
 }
 
 module.exports = wordSearch
